112dd8c0fd1c984bce9e6fb6306c72f16ca4675a
lombre <mboulmbr@gmail.com>
2023-07-18 16:36:48 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,8 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10)
+}
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
@@ -29,5 +30,4 @@ _putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
-}
 }
