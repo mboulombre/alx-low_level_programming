4aae88114bc176456a3d79e79f7b598e61b19df4
lombre <mboulmbr@gmail.com>
2023-07-18 15:34:00 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
+void print_alphabet(void)
+{
+char letre;
+for (letre = 'a'; letre <= 'z'; letre++)
+_putchar(letre);
+
+_putchar('\n');
+}
